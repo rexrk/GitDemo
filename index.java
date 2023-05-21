aa2dbf8c50e055628e82079e90c467cb3a05ff83
RexRK <kraman938@gmail.com>
2023-05-21 11:54:02 +0530

    Index modifies with added methods

---
+++ index.java
@@ -0,0 +1,11 @@
+public class index {
+    index(){
+        String message = "New Message";
+    }
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+        System.out.println(false);
+    }
+
+}
